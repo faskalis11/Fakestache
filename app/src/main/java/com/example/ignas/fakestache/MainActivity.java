e28259a18cab04e66380f3c28ab99097e56e8bfb
Ignas <ignasjankauskas9@gmail.com>
2016-10-31 14:20:27 +0200

    comment

--- app/src/main/java/com/example/ignas/fakestache/MainActivity.java
+++ app/src/main/java/com/example/ignas/fakestache/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.ignas.fakestache;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // gfg hgf
         //Simo komentaras
+
+        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
     }
 }
