f4105cc1c36c0584e3d2c9083eaf09c623730a62
Ignas <ignasjankauskas9@gmail.com>
2016-11-09 18:37:23 +0200

    Camera Intent + icon

--- app/src/main/java/com/example/ignas/fakestache/Gallery.java
+++ app/src/main/java/com/example/ignas/fakestache/Gallery.java
@@ -1,5 +1,6 @@
 package com.example.ignas.fakestache;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,19 +13,22 @@ public class Gallery extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_gallery);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageResource(R.drawable.ic_camera_alt_black_24dp);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(Gallery.this, EditActivity.class);
+                startActivity(intent);
             }
             //Intent Camera st
         });
+
     }
 
 }
