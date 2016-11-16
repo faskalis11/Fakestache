c2854471e083d80e99b46fd44de3d54e7604d258
Ignas <ignasjankauskas9@gmail.com>
2016-11-16 12:35:51 +0200

    ImageAcitivity

--- app/src/main/java/com/example/ignas/fakestache/ImageFragment.java
+++ app/src/main/java/com/example/ignas/fakestache/ImageFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -50,10 +51,12 @@ public class ImageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image, container, false);
+        View view = inflater.inflate(R.layout.fragment_image, container, false);
 
-        path = getArguments().getString("path");
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
 
+        
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
